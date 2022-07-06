317d898e7152d5728d7d5c0a1b4b5e583625fc1a
meikalaheshla <fausto.boshoff@gmail.com>
2022-07-06 17:46:12 +0200

    MILESTONE 1

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// MILESTONE 1 CREO L' ARRAY DI OGGETTI  
+
+const posts = [
+    {
+        id: 1,
+        authorName: Pippo,
+        authorPic: 'https://picsum.photos/id/1/200/300',
+        postDate: 12 / 05 / 2022,
+        postTeaxt: 'Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum',
+        postImg: 'https://picsum.photos/id/100/200/300',
+        likesNumber: 5,
+    },
+    {
+        id: 2,
+        authorName: Pippo,
+        authorPic: 'https://picsum.photos/id/1/200/300',
+        postDate: 12 / 06 / 2022,
+        postTeaxt: 'Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum',
+        postImg: 'https://picsum.photos/id/1001/200/300',
+        likesNumber: 6,
+    },
+    {
+        id: 3,
+        authorName: Pippo,
+        authorPic: 'https://picsum.photos/id/1/200/300',
+        postDate: 12 / 07 / 2022,
+        postTeaxt: 'Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum',
+        postImg: 'https://picsum.photos/id/1002/200/300',
+        likesNumber: 8,
+    }
+]
