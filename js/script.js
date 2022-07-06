5dbee3bcd4a5b41c3dd9598bc881e8c69e2a1e63
meikalaheshla <fausto.boshoff@gmail.com>
2022-07-06 18:31:15 +0200

    better indentation in js

--- js/script.js
+++ js/script.js
@@ -38,39 +38,45 @@ for (let i = 0; i < posts.length; i++) {
     const post = posts[i];
 
     postCard += `
-    <div class="post">
-        <div class="post__header">
-          <div class="post-meta">
-            <div class="post-meta__icon">
-              <img class="profile-pic" src=${post.postImg} alt="Phil Mangione" />
+        <div class="post">
+            <div class="post__header">
+                 <div class="post-meta">
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src=${post.postImg} alt="Phil Mangione" />
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${post.authorName}</div>
+                         <div class="post-meta__time">${post.postDate}</div>
+                    </div>
+                </div>
             </div>
-            <div class="post-meta__data">
-              <div class="post-meta__author">${post.authorName}</div>
-              <div class="post-meta__time">${post.postDate}</div>
+            <div class="post__text">
+                ${post.postTeaxt}
             </div>
-          </div>
-        </div>
-        <div class="post__text">
-         ${post.postTeaxt}
-        </div>
-        <div class="post__image">
-          <img src=${post.postImg} alt="" />
-        </div>
-        <div class="post__footer">
-          <div class="likes js-likes">
-            <div class="likes__cta">
-              <a class="like-button js-like-button" href="#" data-postid="1">
-                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                <span class="like-button__label">Mi Piace</span>
-              </a>
+            <div class="post__image">
+                <img src=${post.postImg} alt="" />
             </div>
-            <div class="likes__counter">Piace a <b id="like-counter-1" class="js-likes-counter">${post.likesNumber}</b> persone</div>
-          </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">Piace a <b id="like-counter-1" class="js-likes-counter">${post.likesNumber}</b> persone</div>
+                 </div>
+            </div>
         </div>
-      </div>
-    </div>
-
-    `
+        
+            
+            `
 
 }
 postContainer.innerHTML = postCard
+
+const likeButton = document.querySelector('js-like-button');
+
+likeButton.addEventListener(click, function () {
+    likeButton.classList.add('like-button--liked')
+})
